caa00c9cc65578e28caacf607e3d70aaa588a9e0
Ethan John Paguntalan <dev.ejohnpaguntalan@gmail.com>
2023-12-05 11:48:10 -0800

    Add const lesson

---
+++ variables_and_data_types/bin/example4.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  String name = "String";
+  var address = "";
+
+  address = name;
+  print(address); // String
+  print(name); // Sting
+}
---
+++ variables_and_data_types/bin/example5.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  final age = 10;
+
+  // constant cannot be assigned a non-constant value
+  // const age2 = age; -> INVALID
+  print(age);
+}
