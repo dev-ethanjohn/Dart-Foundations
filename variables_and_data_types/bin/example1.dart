26c14086d8f49c6c2d82a3dd15eed809c010ad8b
Ethan John Paguntalan <dev.ejohnpaguntalan@gmail.com>
2023-12-05 09:24:06 -0800

    Explain const vs final keyword

--- variables_and_data_types/bin/example1.dart
+++ variables_and_data_types/bin/example1.dart
@@ -1,4 +1,18 @@
 void main(List<String> args) {
+  // `const` prevents you from chanc
   const name = "Foo";
   print(name);
 }
+
+// void main() {
+//   int num = 50; // 'num' is initially assigned the value 50
+//   int initialResult =
+//       num * 9; // initialResult is computed based on 'num' value (450 initially)
+//   print('Initial Result: $initialResult'); // Output: Initial Result: 450
+
+//   // Later in the code...
+//   num = 20; // 'num' is reassigned a new value (20)
+//   int updatedResult = num *
+//       9; // updatedResult is computed based on the updated 'num' value (180 now)
+//   print('Updated Result: $updatedResult'); // Output: Updated Result: 180
+// }
--- variables_and_data_types/bin/example2.dart
+++ variables_and_data_types/bin/example2.dart
@@ -1,1 +1,9 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // `final` avoids you to reassign a new value to your variable.
+  // `const` prevents you to reassign + change the value through operation/logic on compile time.
+  final age = [1, 2, 3];
+  print(age); // [1,2,3]
+
+  age.removeAt(0);
+  print(age); // [2,3]
+}
